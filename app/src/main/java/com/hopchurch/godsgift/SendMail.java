6b9ed3fe4157a1fa1960df2ddd12a551f6d10d56
Vijey Karrun <31200216+vijeykarrun@users.noreply.github.com>
2017-09-19 14:53:01 +0530

    Add files via upload

---
+++ app/src/main/java/com/hopchurch/godsgift/SendMail.java
@@ -0,0 +1,96 @@
+package com.hopchurch.godsgift;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+
+class SendMail extends AsyncTask<Void,Void,Void> {
+
+
+    private Context context;
+    private Session session;
+
+
+    private String email;
+    private String subject;
+    private String message;
+
+
+    private ProgressDialog progressDialog;
+
+
+    public SendMail(Context context, String email){
+
+        this.context = context;
+        this.email = email;
+
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+         progressDialog = ProgressDialog.show(context,"Verifying Mail","Please wait...",false,false);
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        progressDialog.dismiss();
+
+        Toast.makeText(context,"Message Sent",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        Properties props = new Properties();
+
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+
+
+        session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(config.EMAIL, config.PASSWORD);
+                    }
+                });
+
+        try {
+
+            MimeMessage mm = new MimeMessage(session);
+
+
+            mm.setFrom(new InternetAddress(config.EMAIL));
+
+            mm.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
+
+            mm.setSubject("Welcome to GIFT OF GOD");
+
+            mm.setText("Praise the LORD , You have successfully loged in into Gift of God app");
+
+
+            Transport.send(mm);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ app/src/main/java/com/hopchurch/godsgift/config.java
@@ -0,0 +1,10 @@
+package com.hopchurch.godsgift;
+
+
+
+public class config {
+
+    public static final String EMAIL ="hopchurchch@gmail.com";
+    public static final String PASSWORD ="jesushopchurch";
+
+}
